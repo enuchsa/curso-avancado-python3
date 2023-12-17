ead2e90b4a91a226fbd07620df0f0ddcdff16812
enuchsa <enuchsa@gmail.com>
2023-12-17 16:16:00 -0300

    string

---
+++ python_basico/str.py
@@ -0,0 +1,15 @@
+print(1234)
+
+# Aspas simples
+print('Enuch Santos')
+print('Enuch "Santos"')
+
+# Aspas duplas
+print("Erik Rodrigues")
+print("Erik 'Rodrigues'")
+
+# Escape
+print("Vitória \"Marianna\"") 
+
+# r
+print(r"Lucas \"Motano\"") 
