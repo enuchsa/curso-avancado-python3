00f8b9f0ea6896ea3233932ea7ab98dfb60afa37
enuchsa <enuchsa@gmail.com>
2023-12-19 13:47:23 -0300

    variables

---
+++ python_basico/variables.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas
+# pode usar após as letras números e underline _.
+
+nome_completo = 'Enuch Santos de Araújo Júnior'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
+
+# Uso de variáveis para evitar repetir código, reaproveitando código já criado.
+int_um = int(1)
+print(int_um, type(int_um))
+
+# Clean Code: nomes de varáveis devem fazer sentido com o valor que recebem.
+nome = 'Erik'
+idade = 22
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior de idade?: ', maior_de_idade)
