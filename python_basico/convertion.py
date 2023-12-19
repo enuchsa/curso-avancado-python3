e1eb18465f328e00f719d94f322566796a7741b9
enuchsa <enuchsa@gmail.com>
2023-12-18 23:45:50 -0300

    coercion and typecasting

---
+++ python_basico/convertion.py
@@ -0,0 +1,11 @@
+# Conversão de tipos
+# Tipos imutável e primitivos:
+# str, int, float, bool
+
+print(1 + 1) # Sum
+print('a' + 'b') # Concat
+# print(1 + '1') # error
+
+print(int('1'), type(int('1'))) # ex de coerção
+print(int('1') + 1)
+print(float(1.5) + 1) # o inteiro é convertido para um float
