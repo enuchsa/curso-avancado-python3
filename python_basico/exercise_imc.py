2392afa2f3a8a7cd11b2cd464e14dcaa5bd7da5d
enuchsa <enuchsa@gmail.com>
2023-12-19 19:57:54 -0300

    exercise IMC

---
+++ python_basico/exercise_imc.py
@@ -0,0 +1,8 @@
+nome = 'Enuch Santos'
+altura = 1.73
+peso = 55.5
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura')
+print('pesa:', peso, 'quilos')
+print('e seu IMC é:', imc)
