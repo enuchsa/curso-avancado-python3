95969675f831e58f58cb5051f0c4824539103d40
enuchsa <enuchsa@gmail.com>
2023-12-19 14:40:21 -0300

    concatenation and repetition with strings

---
+++ python_basico/concatenation_repetition.py
@@ -0,0 +1,10 @@
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_enuch = 3 * 'Enuch'
+print(a_dez_vezes)
+print(tres_vezes_enuch)
+
+nome_sobrenome = 'Enuch' + ' ' + 'Santos'
+print(nome_sobrenome)
