b70982d2f38bf39a2247e1ae5ce938dc177c8d4a
enuchsa <enuchsa@gmail.com>
2023-12-19 20:13:50 -0300

    f-strings introducion

---
+++ python_basico/introducion_f-string.py
@@ -0,0 +1,12 @@
+nome = 'Enuch'
+sentimentos = 'perdido'
+altura = 1.73
+vontade = 'desaparecer'
+
+frase = f'O {nome} tem {altura:.2f} de altura e tem sentido-se {sentimentos} \nsua vontade é de {vontade}'
+
+print(frase)
+
+# Formatando números
+real = 1005.39
+print(f'seu troco é de {real:,.2f} reais')
