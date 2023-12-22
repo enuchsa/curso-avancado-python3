fbbe292c240903197181d481ac8ea64f0b49b455
enuchsa <enuchsa@gmail.com>
2023-12-22 12:45:30 -0300

    'OR' and 'NOT'

---
+++ python_basico/logic_operators.py
@@ -0,0 +1,24 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições precisam ser verdadeiras
+# Verdadeiras.
+
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '12345'
+
+# if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+    
+# Tabela lógica
+# print(True and False and True) # Exibe o primeiro valor falso
+# print(0 or False or 0 or 'abc') # Exibe o primeiro valor verdadeiro
+
+# senha = input('Digite uma senha: ') or 'Sem senha'
+# print(senha)
+
+# not
+print(not True)
