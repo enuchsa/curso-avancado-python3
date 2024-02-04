df9d4dc72e708a11b4da41fdba5184a574f6d3e8
enuchsa <enuchsa@gmail.com>
2024-02-04 20:07:52 -0300

    lists insert

--- python_basico/list_methods.py
+++ python_basico/list_methods.py
@@ -4,3 +4,16 @@ lista[2] = 10
 
 print(lista)
 print(lista[2])
+
+del lista [2] # deletando o valor do indice 2
+
+print(lista)
+
+lista.append(50)
+
+print(lista)
+removido = lista.pop()
+print(removido)
+
+lista.insert(2, "sexo") # inserindo um dado pelo indice
+print(lista)
