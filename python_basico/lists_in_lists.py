0b86dbf7520938fac00424968e10f762a0a4b4d9
enuchsa <enuchsa@gmail.com>
2024-02-05 18:30:46 -0300

    listas em listas

---
+++ python_basico/lists_in_lists.py
@@ -0,0 +1,13 @@
+salas = [
+    # 0         1
+    ['maria', 'helena'], # 0
+    # 0
+    ['enuch', ], # 1
+    # 0             1       2
+    ['natalea', 'loaana', 'lux', (0, 10, 20, 30, 40)] # 2
+]
+
+for sala in salas:
+    print(sala)
+    
+print(salas[2][3][2])
